71f2fac5c1bf8fd848b40ab150ea236fadc6cc2a
LineseWorld <1076452761@qq.com>
2024-05-12 20:53:44 +0800

    feat: 更新脚本逻辑，一次启动可多次输入多场赛事

--- main.py
+++ main.py
@@ -91,77 +91,82 @@ def update_played_game(db: FootballMatchesDatabase, crawler: DongqiudiCrawler):
 
 
 def generate_game_match(db: FootballMatchesDatabase, crawler: DongqiudiCrawler):
-    match_id = input("输入比赛id：")
-    g_game_detail = crawler.get_game_detail(match_id)
 
-    # print(type(g_game_detail))
-    header_info = GetHeaderInfo(g_game_detail)
+    while True:
+        match_id = input("输入比赛id：")
+        if match_id == "end":
+            break
+        g_game_detail = crawler.get_game_detail(match_id)
 
-    analysis_init_data = GetAnalysisInitData(g_game_detail)
+        # print(type(g_game_detail))
+        header_info = GetHeaderInfo(g_game_detail)
 
-    com1 = Competition(header_info, analysis_init_data)
+        analysis_init_data = GetAnalysisInitData(g_game_detail)
 
-    com_info = com1.GenerateTemplate()
-    print(com_info["paper_title"])
-    pre = Prediction(com1)
-    # pre.GetWinPrediction()
-    pre_info = pre.GetGoalPrediction()
+        com1 = Competition(header_info, analysis_init_data)
 
-    print("【笔者推荐】")
-    author_info = dict()
-    author_win = input("主队胜负推荐：")
-    author_goal = input("进球数推荐：")
-    author_score = input("比分推荐：")
-    author_info["win_text"] = author_win
-    author_info["goal_number_text"] = author_goal
-    author_info["goal_text"] = author_score
+        com_info = com1.GenerateTemplate()
+        print(com_info["paper_title"])
+        pre = Prediction(com1)
+        # pre.GetWinPrediction()
+        pre_info = pre.GetGoalPrediction()
 
-    p = Poster()
-    p.match_name = com_info["match_name"]
-    p.match_time = com_info["match_time"]
+        print("【笔者推荐】")
+        author_info = dict()
+        author_win = input("主队胜负推荐：")
+        author_goal = input("进球数推荐：")
+        author_score = input("比分推荐：")
+        author_info["win_text"] = author_win
+        author_info["goal_number_text"] = author_goal
+        author_info["goal_text"] = author_score
 
-    home_team = com_info["home_team"]
-    p.team1_name = home_team["name"]
-    p.team1_icon = "team1_icon.png"
-    download.download_picture(home_team["team_logo_url"], "team1_icon.png")
+        p = Poster()
+        p.match_name = com_info["match_name"]
+        p.match_time = com_info["match_time"]
 
-    away_team = com_info["away_team"]
-    p.team2_name = away_team["name"]
-    p.team2_icon = "team2_icon.png"
-    download.download_picture(away_team["team_logo_url"], "team2_icon.png")
+        home_team = com_info["home_team"]
+        p.team1_name = home_team["name"]
+        p.team1_icon = "team1_icon.png"
+        download.download_picture(home_team["team_logo_url"], "team1_icon.png")
 
-    # 插入数据
-    # 字符串日期时间
-    date_string = com_info["match_time"]
+        away_team = com_info["away_team"]
+        p.team2_name = away_team["name"]
+        p.team2_icon = "team2_icon.png"
+        download.download_picture(away_team["team_logo_url"], "team2_icon.png")
 
-    # 将字符串转换为datetime对象
-    dt_object = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
+        # 插入数据
+        # 字符串日期时间
+        date_string = com_info["match_time"]
 
-    # 将datetime对象转换为时间戳
-    timestamp = dt_object.timestamp()
-    match_data = {
-        'match_id': match_id,
-        'match_time': com_info["match_time"],
-        'match_time_stamp': timestamp,
-        'match_title': com_info["match_name"],
-        'team_a_name': home_team["name"],
-        'team_b_name': away_team["name"],
-        'team_a_info': json.dumps(home_team, ensure_ascii=False),
-        'team_b_info': json.dumps(away_team, ensure_ascii=False),
-        'ai_predict_info': json.dumps(pre_info),
-        'author_predict_win': author_win,
-        'author_predict_goal_num': author_goal,
-        'author_predict_score': author_score,
-    }
-    db.insert_or_replace_match(match_data)
-    print("insert db success, data:[{}]".format(match_data))
+        # 将字符串转换为datetime对象
+        dt_object = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
 
-    p.show(com_info, pre_info, author_info)
+        # 将datetime对象转换为时间戳
+        timestamp = dt_object.timestamp()
+        match_data = {
+            'match_id': match_id,
+            'match_time': com_info["match_time"],
+            'match_time_stamp': timestamp,
+            'match_title': com_info["match_name"],
+            'team_a_name': home_team["name"],
+            'team_b_name': away_team["name"],
+            'team_a_info': json.dumps(home_team, ensure_ascii=False),
+            'team_b_info': json.dumps(away_team, ensure_ascii=False),
+            'ai_predict_info': json.dumps(pre_info),
+            'author_predict_win': author_win,
+            'author_predict_goal_num': author_goal,
+            'author_predict_score': author_score,
+        }
+        db.insert_or_replace_match(match_data)
+        print("insert db success, data:[{}]".format(match_data))
+
+        p.show(com_info, pre_info, author_info)
 
 
 if __name__ == '__main__':
     with FootballMatchesDatabase() as db, DongqiudiCrawler() as crawler:
         # 更新过往比赛才开启
         # update_played_game(db, crawler)
+
 
         generate_game_match(db, crawler)
